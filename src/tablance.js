3bd307e3c6c80ee6eb25f4231334eec0c0daa313
Oscar Jonsson <oscar@spadrig.se>
2022-12-01 13:22:29 +0100

    Added negative offset to cellcursor so its always fully visible on edge-cells

--- src/tablance.js
+++ src/tablance.js
@@ -1631,6 +1631,7 @@ class Tablance {
 		this.#exitEditMode(true);
 		this.#multiCellSelected=isMultiCell;
 		this.#adjustCursorPosSize(cellEl);
+		this.#cellCursor.classList.toggle("expansion",cellEl.closest("tr.expansion"));
 		this.#cellCursor.classList.toggle("disabled",cellEl.classList.contains("disabled"));
 		(isMultiCell?this.#multiRowArea.firstChild:this.#scrollingContent).appendChild(this.#cellCursor);
 		this.#selectedCell=cellEl;
